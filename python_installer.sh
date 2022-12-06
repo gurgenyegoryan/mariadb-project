93b84f714ad23daaa9c27a53520e354e8f28aaee
gurgen.yegoryan <gurgen.yegoryan@gmail.com>
2022-12-06 13:55:33 +0400

    Modified python installer

--- python_installer.sh
+++ python_installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt update
-apt install software-properties-common
+apt install software-properties-common -y
 add-apt-repository ppa:deadsnakes/ppa -y
 apt install python3.9 python3-pip -y
