9ecea554da6ed56504de9d70142cbad1be0ce754
gurgen.yegoryan <gurgen.yegoryan@gmail.com>
2022-12-06 14:39:29 +0400

    Modifieds

--- insert_db.py
+++ insert_db.py
@@ -19,12 +19,9 @@ columns_name_string = ','.join([str(item) for item in columns_name])
 rows_count = csv_get.rows_count
 csv_data = csv_get.data
 
-counter = 0
-start = 0
-
 
 def insert_process(data, tb_name, cl_name_string, vl_count):
-    global counter, current, start
+    global counter, current
 
     start = time.time()
     current = datetime.datetime.now()
@@ -33,8 +30,6 @@ def insert_process(data, tb_name, cl_name_string, vl_count):
     for row in data:
         try:
             cur.execute(f"INSERT INTO {tb_name} ({cl_name_string}) VALUES ({vl_count})", row)
-            counter += 1
-            # print('row set: ')
         except mariadb.Error as e:
             print(f"Error: {e}")
 
@@ -51,15 +46,14 @@ def insert_process(data, tb_name, cl_name_string, vl_count):
 
 
 def get_using_ram():
-    ram_list = []
-    file = open("text.txt", "a")
-    while connector.cur:
+    file = open("ram_usage_file", "a")
+    while cur:
         ram_usage = psutil.virtual_memory()[3] / 1000000000
         file.write(str(ram_usage) + '\n')
-        time.sleep(0.1)
+        time.sleep(1)
     file.close()
 
 
 threading.Thread(target=insert_process, args=(csv_data, table_name, columns_name_string, values_count)).start()
 # threading.Thread(target=counter_rows()).start()
-threading.Thread(target=get_using_ram())
+threading.Thread(get_using_ram()).start()
--- python_installer.sh
+++ python_installer.sh
@@ -4,3 +4,4 @@ apt update
 apt install software-properties-common -y
 add-apt-repository ppa:deadsnakes/ppa -y
 apt install python3.9 python3-pip -y
+pip3 install psutil
