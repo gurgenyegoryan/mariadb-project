37d834408248f5b433f422efb5b61326526edaeb
gurgen.yegoryan <gurgen.yegoryan@gmail.com>
2022-12-06 13:37:13 +0400

    Modified python installer

--- python_installer.sh
+++ python_installer.sh
@@ -5,4 +5,4 @@ apt update\
 && add-apt-repository ppa:deadsnakes/ppa -y \
 && apt install python3.9 -y \
 && apt install -y python3-pip \
-&& apt install -y python3-pip 
+&& apt install -y python3-pip
