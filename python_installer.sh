6283d4d0a6bd234e879e9625e4fe92d1e95c99cd
gurgen.yegoryan <gurgen.yegoryan@gmail.com>
2022-12-06 13:35:59 +0400

    Modified Dockerfile

--- python_installer.sh
+++ python_installer.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-apt update
-apt install software-properties-common
-add-apt-repository ppa:deadsnakes/ppa -y
-apt install python3.9 -y
-<<<<<<< HEAD
-apt install -y python3-pip
-=======
-apt install -y python3-pip
->>>>>>> 590b5efe680153aa9e9d84a8ba4186df198d6ef7
+apt update\
+&& install software-properties-common \
+&& add-apt-repository ppa:deadsnakes/ppa -y \
+&& apt install python3.9 -y \
+&& apt install -y python3-pip \
+&& apt install -y python3-pip 
